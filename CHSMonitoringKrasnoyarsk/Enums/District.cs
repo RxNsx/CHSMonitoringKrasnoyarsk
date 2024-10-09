055dd8e33573a0a9ce50e842e696261e13b7250e
Vadim Sbitnev <vadim.sbitnev@streamlabs.ru>
2024-10-09 08:20:03 +0300

    Добавляет районы в enum и класс расширения для получения описания

---
+++ CHSMonitoringKrasnoyarsk/Enums/District.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace CHSMonitoringKrasnoyarsk.Enums;
+
+public enum District
+{
+    [Description("Октябрьский")]
+    Okt = 1,
+    [Description("Железнодорожный")]
+    Jlz = 2,
+    [Description("Центральный")]
+    Cen = 3,
+    [Description("Советский")]
+    Sov = 4,
+    [Description("Ленинский")]
+    Len = 5,
+    [Description("Кировский")]
+    Kir = 6,
+    [Description("Свердловский")]
+    Sve = 7
+}
