462c970eefb590bb0b3cc2bddcdec167af0e47f1
Vadim Sbitnev <vadim.sbitnev@streamlabs.ru>
2025-01-06 16:58:37 +0300

    Выносит логику редактирования текста после парсинга в метод расширения

--- CHSMonitoringKrasnoyarsk/Extensions/TextExtensions.cs
+++ CHSMonitoringKrasnoyarsk/Extensions/TextExtensions.cs
@@ -12,8 +12,36 @@ public static class TextExtensions
     /// </summary>
     /// <param name="text"></param>
     /// <returns></returns>
-    public static string NormalizeSpacesInText(this string text)
+    public static string NormalizeText(this string text)
     {
+        text = text.Trim();
+            
+        var newLineSymbolsCount = Regex.Matches(text, "\n").Count;
+        while (newLineSymbolsCount > 1)
+        {
+            if (string.IsNullOrEmpty(text) &&
+                text == "&nbsp;")
+            {
+                continue;
+            }
+                        
+            text =  text.Replace("\n", "");
+            newLineSymbolsCount--;
+        }
+                    
+        var carriageSymbolsCount = Regex.Matches(text, "\r").Count;
+        while (carriageSymbolsCount > 0)
+        {
+            if (string.IsNullOrEmpty(text) &&
+                text == "&nbsp;")
+            {
+                continue;
+            }
+                        
+            text =  text.Replace("\r", "");
+            carriageSymbolsCount--;
+        }
+        
         return Regex.Replace(text, @"\s+", " ");
     }
 }
