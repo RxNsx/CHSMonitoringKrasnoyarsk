6a630429f221b347ae23a29f9cf2d77289433add
Vadim Sbitnev <vadim.sbitnev@streamlabs.ru>
2025-01-15 19:05:51 +0300

    Добавлены улицы на букву А

---
+++ CHSMonitoringKrasnoyarsk/Enums/StreetNameEnum.cs
@@ -0,0 +1,103 @@
+﻿namespace CHSMonitoringKrasnoyarsk.Enums;
+
+using System.ComponentModel;
+
+using System.ComponentModel;
+
+public enum StreetNameEnum
+{
+    [Description("Абазинский")]
+    Abazinskiy,
+    [Description("Абаканский")]
+    Abakanskiy,
+    [Description("Абытаевская")]
+    Abytaevskaya,
+    [Description("Авамский")]
+    Avamskiy,
+    [Description("Авангардная")]
+    Avangardnaya,
+    [Description("Авиагородок")]
+    Aviogorodok,
+    [Description("Авиаторов")]
+    Aviatory,
+    [Description("Авиационная")]
+    Aviatsionnaya,
+    [Description("Автобусный")]
+    Avtobusnyy,
+    [Description("Автомобилистов")]
+    Avtomobilistov,
+    [Description("Адмирала Макарова")]
+    AdmiralaMakarova,
+    [Description("Ады Лебедевой")]
+    AdyLebedevoi,
+    [Description("Азовская")]
+    Azovskaya,
+    [Description("Айвазовского")]
+    Ayvazovskogo,
+    [Description("Академгородок")]
+    Akademgorodok,
+    [Description("Академика Вавилова")]
+    AkademikaVavilova,
+    [Description("Академика Киренского")]
+    AkademikaKirenskogo,
+    [Description("Академика Павлова")]
+    AkademikaPavlova,
+    [Description("Аксеновского")]
+    Aksenovskogo,
+    [Description("Акционерная")]
+    Aktsionernaya,
+    [Description("Александра Матросова")]
+    AleksandraMatrosova,
+    [Description("Александровский")]
+    Aleksandrovskiy,
+    [Description("Алексеева")]
+    Alekseeva,
+    [Description("Алексея Асеева")]
+    AlekseyaAseeva,
+    [Description("Алеши Тимошенкова")]
+    AleshTimoshenkova,
+    [Description("Алтайская")]
+    Altayskaya,
+    [Description("6-я Алыкельская")]
+    _6yaAlykelSkaya,
+    [Description("Алюминиевая")]
+    Alyumineevaya,
+    [Description("Аманатский")]
+    Amanatskiy,
+    [Description("Амурская")]
+    Amurskaya,
+    [Description("Анабарская")]
+    Anabarskaya,
+    [Description("Анатолия Гладкова")]
+    AnatoliyaGladkova,
+    [Description("Ангарская")]
+    Angarskaya,
+    [Description("Анзельская")]
+    AnzelSkaya,
+    [Description("Апрельская")]
+    AprelSkaya,
+    [Description("Аральская")]
+    AralSkaya,
+    [Description("Арейская")]
+    Areyskaya,
+    [Description("Аринская")]
+    Arinskaya,
+    [Description("Армейская")]
+    Armeyskaya,
+    [Description("Арский")]
+    Arskiy,
+    [Description("Артемовская")]
+    Artemovskaya,
+    [Description("Архангельская")]
+    ArkhangelSkaya,
+    [Description("Астраханская")]
+    Astrakhanskaya,
+    [Description("Атаманская")]
+    Atamanskaya,
+    [Description("Афонтовский")]
+    Afontovskiy,
+    [Description("Ачинский")]
+    Achinskiy,
+    [Description("Аэровокзальная")]
+    Aerovokzalnaya
+}
