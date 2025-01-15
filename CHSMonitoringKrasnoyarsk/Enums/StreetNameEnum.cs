6ca6c181c4a0a99337bbc20e0bb104a1a8f82acc
Vadim Sbitnev <vadim.sbitnev@streamlabs.ru>
2025-01-15 19:12:24 +0300

    Добавлены улицы на букву Б

--- CHSMonitoringKrasnoyarsk/Enums/StreetNameEnum.cs
+++ CHSMonitoringKrasnoyarsk/Enums/StreetNameEnum.cs
@@ -6,6 +6,8 @@ using System.ComponentModel;
 
 public enum StreetNameEnum
 {
+    #region Улицы на букву A
+    
     [Description("Абазинский")]
     Abazinskiy,
     [Description("Абаканский")]
@@ -99,5 +101,136 @@ public enum StreetNameEnum
     [Description("Ачинский")]
     Achinskiy,
     [Description("Аэровокзальная")]
-    Aerovokzalnaya
+    Aerovokzalnaya,
+    
+    #endregion
+    
+    #region Улицы на букву Б
+    
+    [Description("Бабушкина")]
+    Babushkina,
+    [Description("Бадалык")]
+    Badalyk,
+    [Description("Бадалыкская")]
+    Badalykskaya,
+    [Description("Базайская")]
+    Bazayskaya,
+    [Description("Базарная")]
+    BazarNaya,
+    [Description("Байкальская")]
+    Baykalskaya,
+    [Description("Байкитская 1-я")]
+    Baykitskaya1,
+    [Description("Байкитская 2-я")]
+    Baykitskaya2,
+    [Description("Балахтинская")]
+    BalakhTinskaya,
+    [Description("Бартатский")]
+    Bartatskiy,
+    [Description("Батурина")]
+    BatUrIna,
+    [Description("Баумана")]
+    BaUmana,
+    [Description("Башиловская")]
+    BashIlovskaya,
+    [Description("Бебеля")]
+    Bebelya,
+    [Description("Бегичева")]
+    Begicheva,
+    [Description("Белинского")]
+    Belinskogo,
+    [Description("Беловежская")]
+    BelovezhSkaya,
+    [Description("Белопольского")]
+    BelopolSkogo,
+    [Description("Белорусская")]
+    Belorusskaya,
+    [Description("Беляева")]
+    Belyaeva,
+    [Description("Березина")]
+    Berezina,
+    [Description("Березовая")]
+    Berezovaya,
+    [Description("Биатлонная")]
+    Biathlonnaya,
+    [Description("Бийская")]
+    Biyskaya,
+    [Description("Бийхемская")]
+    BiyKhemsKaya,
+    [Description("Биологов")]
+    Biologov,
+    [Description("Бирикчульский")]
+    BirikchulSkiy,
+    [Description("Бирилюсская")]
+    Birilyusskaya,
+    [Description("Бирюсинская")]
+    Biryusinskaya,
+    [Description("Благодатная")]
+    Blagodatnaya,
+    [Description("Бобровый")]
+    Bobrovyy,
+    [Description("Богдана Хмельницкого")]
+    BogdanaKhmelNitskogo,
+    [Description("Боголеповская")]
+    Bogolepovskaya,
+    [Description("Боготольский")]
+    BogotolSkiy,
+    [Description("Бограда")]
+    Bograda,
+    [Description("Богучанская")]
+    Boguchanskaya,
+    [Description("Бодрая")]
+    Bodraya,
+    [Description("Бойкая")]
+    Boykaya,
+    [Description("Болдинская")]
+    Boldinskaya,
+    [Description("Большегвардейская")]
+    BolSheGvardeyskaya,
+    [Description("Большой")]
+    BolShoy,
+    [Description("Борисевича")]
+    Borisevicha,
+    [Description("Борисова")]
+    Borisova,
+    [Description("Боровая 1-я")]
+    Borovaya1,
+    [Description("Боровая 2-я")]
+    Borovaya2,
+    [Description("Бородинская")]
+    Borodinskaya,
+    [Description("Борьбы")]
+    BorBy,
+    [Description("Ботаническая")]
+    Botanicheskaya,
+    [Description("Ботаническая 2-я")]
+    Botanicheskaya2,
+    [Description("Ботаническая 3-я")]
+    Botanicheskaya3,
+    [Description("Ботаническая 4-я")]
+    Botanicheskaya4,
+    [Description("Ботанический")]
+    Botanicheskiy,
+    [Description("Боткина")]
+    Botkina,
+    [Description("Бохтинский")]
+    Bokhtinskiy,
+    [Description("Братская 1-я")]
+    Bratskaya1,
+    [Description("Братская 2-я")]
+    Bratskaya2,
+    [Description("Братьев Абалаковых")]
+    BratEyAbalakovykh,
+    [Description("Брестская")]
+    Brestskaya,
+    [Description("Брянская")]
+    Bryanskaya,
+    [Description("Брянская 2-я")]
+    Bryanskaya2,
+    [Description("Бугачевская")]
+    Bugachevskaya,
+    [Description("Быковского")]
+    Bykovskogo,
+    
+    #endregion
 }
