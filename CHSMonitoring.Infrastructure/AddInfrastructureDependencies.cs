f2c26238bbde9744b86ad9c6b34bc89889a5bcaa
Vadim Sbitnev <vadim.sbitnev@streamlabs.ru>
2025-01-19 14:23:01 +0300

    Добавляет в инфраструктуру контекст бд с сущностями

---
+++ CHSMonitoring.Infrastructure/AddInfrastructureDependencies.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CHSMonitoring.Infrastructure;
+
+public static class AddInfrastructureDependencies
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        
+        
+        return services;
+    }
+}
---
+++ CHSMonitoring.Infrastructure/Context/MonitoringDbContext.cs
@@ -0,0 +1,38 @@
+﻿using CHSMonitoring.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CHSMonitoring.Infrastructure.Context;
+
+public class MonitoringDbContext : DbContext
+{
+    public MonitoringDbContext(DbContextOptions<MonitoringDbContext> options) : base(options)
+    {
+        
+    }
+
+    public virtual DbSet<District> Type { get; set; }
+    
+    public virtual DbSet<Permission> Permissions { get; set; }
+    
+    public virtual DbSet<PlannedServiceType> PlannedServiceTypes { get; set; }
+    
+    public virtual DbSet<Profile> Profiles { get; set; }
+    
+    public virtual DbSet<Role> Roles { get; set; }
+    
+    public virtual DbSet<ServiceAddress> ServiceAddresses { get; set; }
+    
+    public virtual DbSet<ServiceType> ServiceTypes { get; set; }
+    
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
