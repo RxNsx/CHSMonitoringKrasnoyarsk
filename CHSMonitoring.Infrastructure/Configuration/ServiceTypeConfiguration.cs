f8b6b705e494c51b5a924c1ff744ed409b39f62d
Vadim Sbitnev <vadim.sbitnev@streamlabs.ru>
2025-01-26 14:15:57 +0300

    Добавляет предустановленные данные для типов обслуживания

--- CHSMonitoring.Infrastructure/Configuration/ServiceTypeConfiguration.cs
+++ CHSMonitoring.Infrastructure/Configuration/ServiceTypeConfiguration.cs
@@ -22,5 +22,19 @@ public class ServiceTypeConfiguration : IEntityTypeConfiguration<ServiceType>
         builder.Property(x => x.Description)
             .HasDefaultValue(string.Empty)
             .HasMaxLength(150);
+
+        PrepareData(builder);
+    }
+
+    public void PrepareData(EntityTypeBuilder<ServiceType> builder)
+    {
+        var serviceTypes = new List<ServiceType>()
+        {
+            new() { Name = "Теплоснабжение", Description = string.Empty, Id = Guid.Parse("913164af-4599-434f-8145-72f2bf937566") },
+            new() { Name = "Холодное водоснабжение", Description = string.Empty, Id = Guid.Parse("9c5ab80d-64d3-4cf4-81d0-751aa37624f9") },
+            new() { Name = "Электроснабжение", Description = string.Empty, Id = Guid.Parse("47e046c3-a591-4f17-ac64-061a337270cf") }
+        };
+        
+        builder.HasData(serviceTypes);
     }
 }
