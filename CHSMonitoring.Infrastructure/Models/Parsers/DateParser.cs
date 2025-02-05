541eadde57f69ebc9c45b04b4fe9f11c9872d60c
Vadim Sbitnev <vadim.sbitnev@streamlabs.ru>
2025-02-05 07:12:32 +0300

    Возвращает корректно дату для 1 предмета в коллекции дат после парсинга

--- CHSMonitoring.Infrastructure/Models/Parsers/DateParser.cs
+++ CHSMonitoring.Infrastructure/Models/Parsers/DateParser.cs
@@ -11,42 +11,51 @@ public static class DateParser
         var dateTo = DateTime.MinValue;
         var dateFromString = string.Empty;   
         var dateToString = string.Empty;
-        
-        if (datesList.Any() && datesList.Count == 2)
-        {
-            var format = "dd MMMM HH-mm";
-            var cultureInfo = new CultureInfo("ru-RU");
 
-            if (!DateTime.TryParseExact(datesList[0], format, cultureInfo, DateTimeStyles.None, out dateFrom))
+        switch (datesList.Count)
+        {
+            case 1:
             {
-                dateFromString = datesList[0];
+                return DateInfo.Create(null, null, datesList.First(), datesList.First());
             }
-            else
+            case 2:
             {
-                dateFromString = dateFrom.ToString(cultureInfo);
-            }
+                var format = "dd MMMM HH-mm";
+                var cultureInfo = new CultureInfo("ru-RU");
+
+                if (!DateTime.TryParseExact(datesList[0], format, cultureInfo, DateTimeStyles.None, out dateFrom))
+                {
+                    dateFromString = datesList[0];
+                }
+                else
+                {
+                    dateFromString = dateFrom.ToString(cultureInfo);
+                }
             
-            if (!DateTime.TryParseExact(datesList[1], format, cultureInfo, DateTimeStyles.None, out dateTo))
-            {
-                dateToString = datesList[1];
-            }
-            else
-            {
-                dateToString = dateTo.ToString(cultureInfo);
-            }
-        }
-
-        DateTime? dateFromParsed = dateFrom;
-        DateTime? dateToParsed = dateTo;
-        if (dateFrom == DateTime.MinValue)
-        {
-            dateFromParsed = null;
-        }
-        if (dateTo == DateTime.MinValue)
-        {
-            dateToParsed = null;
-        }
+                if (!DateTime.TryParseExact(datesList[1], format, cultureInfo, DateTimeStyles.None, out dateTo))
+                {
+                    dateToString = datesList[1];
+                }
+                else
+                {
+                    dateToString = dateTo.ToString(cultureInfo);
+                }
+            
+                DateTime? dateFromParsed = dateFrom;
+                DateTime? dateToParsed = dateTo;
+                if (dateFrom == DateTime.MinValue)
+                {
+                    dateFromParsed = null;
+                }
+                if (dateTo == DateTime.MinValue)
+                {
+                    dateToParsed = null;
+                }
         
-        return DateInfo.Create(dateFromParsed, dateToParsed, dateFromString, dateToString);
+                return DateInfo.Create(dateFromParsed, dateToParsed, dateFromString, dateToString);
+            }
+            default:
+                return DateInfo.Create(null, null, string.Empty, string.Empty);
+        }
     }
 }
