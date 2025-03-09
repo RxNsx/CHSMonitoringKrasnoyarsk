9e85c1179e661a6f32a81f0033a3d2d57fddde48
Vadim Sbitnev <alpendev@mail.ru>
2025-03-09 10:44:30 +0300

    -

--- CHSMonitoring.Application/Queries/Geocode/GetDistrictGeoCoordinatesQueryHandler.cs
+++ CHSMonitoring.Application/Queries/Geocode/GetDistrictGeoCoordinatesQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using CHSMonitoring.Application.Dtos.Geocode;
 using CHSMonitoring.Infrastructure.Interfaces;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using Shared;
 
 namespace CHSMonitoring.Application.Queries.Geocode;
@@ -8,21 +9,25 @@ namespace CHSMonitoring.Application.Queries.Geocode;
 public class GetDistrictGeoCoordinatesQueryHandler : IRequestHandler<GetDistrictGeoCoordinatesQuery, Result<List<ServiceAddressGeoLocationDto>>>
 {
     private readonly IGeocodeService _geocodeService;
+    private readonly ILogger<GetDistrictGeoCoordinatesQuery> _logger;
 
     /// <summary>
     /// Конструктор
     /// </summary>
     /// <param name="geocodeService"></param>
-    public GetDistrictGeoCoordinatesQueryHandler(IGeocodeService geocodeService)
+    /// <param name="logger"></param>
+    public GetDistrictGeoCoordinatesQueryHandler(IGeocodeService geocodeService, ILogger<GetDistrictGeoCoordinatesQuery> logger)
     {
         _geocodeService = geocodeService;
+        _logger = logger;
     }
 
     public async Task<Result<List<ServiceAddressGeoLocationDto>>> Handle(GetDistrictGeoCoordinatesQuery request, CancellationToken cancellationToken)
     {
         var serviceAddressesGeoData =  await _geocodeService.GetServiceAddressGeoDataAsync(request.DistrictId, cancellationToken)
             .ConfigureAwait(false);
-
+        _logger.LogInformation($"Count serviceAddresses: {serviceAddressesGeoData.Count}");
+        
         var serviceAddressGeoLocationList =  serviceAddressesGeoData
             .Select(x => new ServiceAddressGeoLocationDto()
             {
