e9d3a6325d31cca2afc14833a499880634aab0ca
Vadim Sbitnev <alpendev@mail.ru>
2025-03-18 04:27:56 +0300

    Удаляет лишние эндпоинты и запросы

--- CHSMonitoring.Application/Queries/ServiceAddresses/GetLatestList/GetLatestListServiceAddressQueryValidator.cs
+++
@@ -1,8 +0,0 @@
-﻿using FluentValidation;
-
-namespace CHSMonitoring.Application.Queries.ServiceAddresses.GetLatestList;
-
-public class GetLatestListServiceAddressQueryValidator : AbstractValidator<GetLatestListServiceAddressQuery>
-{
-    
-}
